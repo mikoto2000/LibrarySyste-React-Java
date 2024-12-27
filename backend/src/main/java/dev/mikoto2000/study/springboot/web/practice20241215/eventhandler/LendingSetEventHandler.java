11178ececeaff710b3d17dc9be9169dc300177b9
mikoto2000 <mikoto2000@gmail.com>
2024-12-27 20:33:13 +0900

    LendingSet 登録時に貸し出した本が貸出中になるように修正。

---
+++ backend/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/eventhandler/LendingSetEventHandler.java
@@ -0,0 +1,44 @@
+package dev.mikoto2000.study.springboot.web.practice20241215.eventhandler;
+
+import java.util.Optional;
+
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import dev.mikoto2000.study.springboot.web.practice20241215.entity.BookStock;
+import dev.mikoto2000.study.springboot.web.practice20241215.entity.BookStockStatus;
+import dev.mikoto2000.study.springboot.web.practice20241215.entity.LendingSet;
+import dev.mikoto2000.study.springboot.web.practice20241215.repository.BookStockStatusRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RepositoryEventHandler
+@Slf4j
+public class LendingSetEventHandler {
+
+  private BookStockStatusRepository bookStockStatusRepository;
+
+  public LendingSetEventHandler(
+      BookStockStatusRepository bookStockStatusRepository) {
+    this.bookStockStatusRepository = bookStockStatusRepository;
+  }
+
+  @HandleBeforeCreate
+  public void updateBookStatusBeforeLoan(LendingSet lendingSet) {
+    Optional<BookStockStatus> lendingAvailable;
+    if (lendingSet.getLendingStatus().getId() == 1) {
+      // 貸出不可能にする
+      lendingAvailable = bookStockStatusRepository.findById(2L);
+    } else {
+      // 貸出可能にする
+      lendingAvailable = bookStockStatusRepository.findById(1L);
+    }
+    if (lendingSet.getBookStock() != null) {
+      for (BookStock bookStock : lendingSet.getBookStock()) {
+        log.debug("👺KITAYO: {}", bookStock.getBookMaster().getName());
+        lendingAvailable.ifPresent((e) -> bookStock.setBookStockStatus(e));
+      }
+    }
+  }
+}
